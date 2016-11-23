d11f93faf0d37489a2278bc95026b974ddb06825
Joel Falcou <joel.falcou@lri.fr>
2016-11-23 22:42:13 +0100

    Make VMX familly types support sub-cardinal pack

--- include/boost/simd/arch/ppc/vmx/pack_traits.hpp
+++ include/boost/simd/arch/ppc/vmx/pack_traits.hpp
@@ -9,53 +9,86 @@
 #ifndef BOOST_SIMD_ARCH_PPC_VMX_PACK_TRAITS_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_PPC_VMX_PACK_TRAITS_HPP_INCLUDED
 
-#include <boost/simd/detail/brigand.hpp>
-#include <boost/simd/detail/pack_traits.hpp>
-#include <boost/simd/arch/common/simd/abi_of.hpp>
 #include <boost/config.hpp>
-#include <type_traits>
+#include <boost/simd/detail/pack_traits.hpp>
+#include <boost/simd/detail/pack_support.hpp>
 
 namespace boost { namespace simd
 {
-  namespace detail
-  {
-    BOOST_SIMD_DEFINE_PACK_TRAITS(float         ,  4, __vector float          );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int32_t  ,  4, __vector signed   int   );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint32_t ,  4, __vector unsigned int   );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int16_t  ,  8, __vector signed   short );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint16_t ,  8, __vector unsigned short );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int8_t   , 16, __vector signed   char  );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint8_t  , 16, __vector unsigned char  );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(char          , 16, __vector unsigned char  );
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  , 16, __vector signed   char, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t , 16, __vector unsigned char, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         , 16, __vector unsigned char, ::boost::simd::vmx_)
 
-    // Match also the bool funky types from VMX
-    BOOST_SIMD_DEFINE_PACK_TRAITS(float         ,  4, __vector __bool int   );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int32_t  ,  4, __vector __bool int   );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint32_t ,  4, __vector __bool int   );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int16_t  ,  8, __vector __bool short );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint16_t ,  8, __vector __bool short );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int8_t   , 16, __vector __bool char  );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint8_t  , 16, __vector __bool char  );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(char          , 16, __vector __bool char  );
-  }
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  8, __vector signed   short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  8, __vector unsigned short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  8, __vector signed   char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  8, __vector unsigned char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  8, __vector unsigned char , ::boost::simd::vmx_)
 
-  namespace ext
-  {
-    template<typename Enable> struct abi_of<float,4,Enable>
-    {
-      using type = ::boost::simd::vmx_;
-    };
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        ,  4, __vector        float  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t ,  4, __vector signed   int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t,  4, __vector unsigned int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  4, __vector signed   short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  4, __vector unsigned short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  4, __vector signed   char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  4, __vector unsigned char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  4, __vector unsigned char , ::boost::simd::vmx_)
 
-    template<typename T, std::size_t N>
-    struct abi_of<  T, N
-                  , typename std::enable_if<   std::is_integral<T>::value
-                                            && (N*sizeof(T) == 16) && (N > 2)
-                                            >::type
-                  >
-    {
-      using type = ::boost::simd::vmx_;
-    };
-  }
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        ,  2, __vector        float  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t ,  2, __vector signed   int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t,  2, __vector unsigned int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  2, __vector signed   short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  2, __vector unsigned short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  2, __vector signed   char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  2, __vector unsigned char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  2, __vector unsigned char , ::boost::simd::vmx_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        ,  1, __vector        float  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t ,  1, __vector signed   int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t,  1, __vector unsigned int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  1, __vector signed   short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  1, __vector unsigned short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  1, __vector signed   char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  1, __vector unsigned char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  1, __vector unsigned char , ::boost::simd::vmx_)
+
+  // Match also the bool funky types from VMX
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  , 16, __vector __bool char, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t , 16, __vector __bool char, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         , 16, __vector __bool char, ::boost::simd::vmx_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  8, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  8, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  8, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  8, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  8, __vector __bool char , ::boost::simd::vmx_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        ,  4, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t ,  4, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t,  4, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  4, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  4, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  4, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  4, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  4, __vector __bool char , ::boost::simd::vmx_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        ,  2, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t ,  2, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t,  2, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  2, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  2, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  2, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  2, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  2, __vector __bool char , ::boost::simd::vmx_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        ,  1, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t ,  1, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t,  1, __vector __bool int  , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,  1, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,  1, __vector __bool short, ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,  1, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,  1, __vector __bool char , ::boost::simd::vmx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,  1, __vector __bool char , ::boost::simd::vmx_)
 } }
 
 #endif
