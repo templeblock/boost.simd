41a536ddbf6ef115dc88d5927c2da64e56ffb7db
Joel Falcou <joel.falcou@lri.fr>
2016-11-23 22:47:30 +0100

    Make AVX familly types support sub-cardinal pack

--- include/boost/simd/arch/x86/avx/pack_traits.hpp
+++ include/boost/simd/arch/x86/avx/pack_traits.hpp
@@ -11,8 +11,7 @@
 
 #include <boost/config.hpp>
 #include <boost/simd/arch/x86/sse2/pack_traits.hpp>
-#include <boost/simd/detail/brigand.hpp>
-#include <boost/simd/detail/pack_traits.hpp>
+#include <boost/simd/detail/pack_support.hpp>
 
 #if defined __GNUC__ && __GNUC__>=6
 #pragma GCC diagnostic push
@@ -21,43 +20,17 @@
 
 namespace boost { namespace simd
 {
-  namespace detail
-  {
-    BOOST_SIMD_DEFINE_PACK_TRAITS(float         ,  8, __m256 );
-    BOOST_SIMD_DEFINE_PACK_TRAITS(double        ,  4, __m256d);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int64_t  ,  4, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint64_t ,  4, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int32_t  ,  8, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint32_t ,  8, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int16_t  , 16, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint16_t , 16, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int8_t   , 32, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint8_t  , 32, __m256i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(char          , 32, __m256i);
-  }
-
-  namespace ext
-  {
-    template<typename Enable> struct abi_of<float,8,Enable>
-    {
-      using type = ::boost::simd::avx_;
-    };
-
-    template<typename Enable> struct abi_of<double,4,Enable>
-    {
-      using type = ::boost::simd::avx_;
-    };
-
-    template<typename T, std::size_t N>
-    struct abi_of<  T, N
-                  , typename std::enable_if<   std::is_integral<T>::value
-                                            && (N*sizeof(T) == 32)
-                                            >::type
-                  >
-    {
-      using type = ::boost::simd::avx_;
-    };
-  }
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,32, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,32, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,32, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t ,16, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t,16, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float        , 8, __m256 , ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t , 8, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t, 8, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(double       , 4, __m256d, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int64_t , 4, __m256i, ::boost::simd::avx_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint64_t, 4, __m256i, ::boost::simd::avx_)
 } }
 
 #if defined __GNUC__ && __GNUC__>=6
