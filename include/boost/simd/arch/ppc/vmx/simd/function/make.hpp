fa82de216af939565c7bffdb0d3d1430bc054cf1
Joel Falcou <joel.falcou@lri.fr>
2016-11-23 22:14:48 +0100

    Adapt make to PPC sub-packs

--- include/boost/simd/arch/ppc/vmx/simd/function/make.hpp
+++ include/boost/simd/arch/ppc/vmx/simd/function/make.hpp
@@ -10,6 +10,9 @@
 #define BOOST_SIMD_ARCH_PPC_VMX_SIMD_FUNCTION_MAKE_HPP_INCLUDED
 
 #include <boost/simd/detail/overload.hpp>
+#include <boost/simd/meta/is_power_of_2.hpp>
+#include <boost/simd/detail/brigand.hpp>
+#include <boost/simd/detail/nope.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -24,14 +27,24 @@ namespace boost { namespace simd { namespace ext
                           )
   {
     using target_t  = typename Target::type;
+    using v_t       = typename target_t::value_type;
 
-    static_assert ( sizeof...(Values) == target_t::static_size
+    static_assert (     (sizeof...(Values) <= target_t::static_size)
+                    &&  (is_power_of_2_<sizeof...(Values)>::value  )
                   , "boost::simd::make - Invalid number of parameters"
                   );
 
     BOOST_FORCEINLINE target_t operator()(Target const&, Values const&... vs) const BOOST_NOEXCEPT
     {
-      typename target_t::storage_type that = { static_cast<typename target_t::value_type>(vs)... };
+      return do_( brigand::range<int,sizeof...(Values), (16/sizeof(v_t))>{}, vs...);
+    }
+
+    template<typename... Z> BOOST_FORCEINLINE
+    static target_t do_(brigand::list<Z...> const&, Values const&... vs) BOOST_NOEXCEPT
+    {
+      using s_t = typename target_t::storage_type;
+
+      s_t that = { static_cast<v_t>(vs)..., detail::nope<v_t,Z>()... };
       return that;
     }
   };
