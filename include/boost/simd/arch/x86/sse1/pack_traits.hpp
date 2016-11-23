5fbf0d69162f6479f0dc8bdfa6abbd6bc0f67248
Joel Falcou <joel.falcou@lri.fr>
2016-11-23 21:47:34 +0100

    Make SSE familly types support sub-cardinal pack

--- include/boost/simd/arch/x86/sse1/pack_traits.hpp
+++ include/boost/simd/arch/x86/sse1/pack_traits.hpp
@@ -1,23 +1,16 @@
 //==================================================================================================
-/*!
-  @file
-
-  Defines SSE1 functions for SIMD storage used by pack
-
-  @copyright 2016 NumScale SAS
+/**
+  Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-
 **/
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_X86_SSE1_PACK_TRAITS_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_X86_SSE1_PACK_TRAITS_HPP_INCLUDED
 
 #include <boost/config.hpp>
-#include <boost/simd/detail/brigand.hpp>
-#include <boost/simd/detail/pack_traits.hpp>
-#include <boost/simd/arch/common/simd/abi_of.hpp>
+#include <boost/simd/detail/pack_support.hpp>
 
 #if defined __GNUC__ && __GNUC__>=6
 #pragma GCC diagnostic push
@@ -26,24 +19,9 @@
 
 namespace boost { namespace simd
 {
-  namespace detail
-  {
-    BOOST_SIMD_DEFINE_PACK_TRAITS(float, 4, __m128);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(float, 2, __m128);
-  }
-
-  namespace ext
-  {
-    template<typename Enable> struct abi_of<float,4,Enable>
-    {
-      using type = ::boost::simd::sse_;
-    };
-
-    template<typename Enable> struct abi_of<float,2,Enable>
-    {
-      using type = ::boost::simd::sse_;
-    };
-  }
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float, 4, __m128, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float, 2, __m128, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(float, 1, __m128, ::boost::simd::sse_)
 } }
 
 #if defined __GNUC__ && __GNUC__>=6
--- include/boost/simd/arch/x86/sse2/pack_traits.hpp
+++ include/boost/simd/arch/x86/sse2/pack_traits.hpp
@@ -1,14 +1,9 @@
 //==================================================================================================
-/*!
-  @file
-
-  Defines SSE2 functions for SIMD storage used by pack
-
-  @copyright 2016 NumScale SAS
+/**
+  Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-
 **/
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_X86_SSE2_PACK_TRAITS_HPP_INCLUDED
@@ -16,8 +11,7 @@
 
 #include <boost/config.hpp>
 #include <boost/simd/arch/x86/sse1/pack_traits.hpp>
-#include <boost/simd/detail/pack_traits.hpp>
-#include <type_traits>
+#include <boost/simd/detail/pack_support.hpp>
 
 #if defined __GNUC__ && __GNUC__>=6
 #pragma GCC diagnostic push
@@ -26,37 +20,45 @@
 
 namespace boost { namespace simd
 {
-  namespace detail
-  {
-    BOOST_SIMD_DEFINE_PACK_TRAITS(double        ,  2, __m128d);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int64_t  ,  2, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint64_t ,  2, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int32_t  ,  4, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint32_t ,  4, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int16_t  ,  8, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint16_t ,  8, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::int8_t   , 16, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(std::uint8_t  , 16, __m128i);
-    BOOST_SIMD_DEFINE_PACK_TRAITS(char          , 16, __m128i);
-  }
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  ,16, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t ,16, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         ,16, __m128i, ::boost::simd::sse_)
 
-  namespace ext
-  {
-    template<typename Enable> struct abi_of<double,2,Enable>
-    {
-      using type = ::boost::simd::sse_;
-    };
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t , 8, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t, 8, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  , 8, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t , 8, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         , 8, __m128i, ::boost::simd::sse_)
 
-    template<typename T, std::size_t N>
-    struct abi_of<  T, N
-                  , typename std::enable_if<   std::is_integral<T>::value
-                                            && (N*sizeof(T) == 16)
-                                            >::type
-                  >
-    {
-      using type = ::boost::simd::sse_;
-    };
-  }
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t , 4, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t, 4, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t , 4, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t, 4, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  , 4, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t , 4, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         , 4, __m128i, ::boost::simd::sse_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(double       , 2, __m128d, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int64_t , 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint64_t, 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t , 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t, 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t , 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t, 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  , 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t , 2, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         , 2, __m128i, ::boost::simd::sse_)
+
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(double       , 1, __m128d, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int64_t , 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint64_t, 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int32_t , 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint32_t, 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int16_t , 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint16_t, 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::int8_t  , 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(std::uint8_t , 1, __m128i, ::boost::simd::sse_)
+  BOOST_SIMD_ENABLE_PACK_SUPPORT(char         , 1, __m128i, ::boost::simd::sse_)
 } }
 
 #if defined __GNUC__ && __GNUC__>=6
