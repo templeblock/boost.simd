0f94922e2cc24b501d9b51b403091f87ea2e214c
jtlap <jtlapreste@gmail.com>
2016-12-16 10:12:30 +0100

    include for amd exp file dispatch

--- include/boost/simd/function/simd/exp.hpp
+++ include/boost/simd/function/simd/exp.hpp
@@ -13,5 +13,9 @@
 #include <boost/simd/function/scalar/exp.hpp>
 #include <boost/simd/arch/common/generic/function/autodispatcher.hpp>
 #include <boost/simd/arch/common/simd/function/exp.hpp>
-
+#if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
+#  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE2_VERSION
+#    include <boost/simd/arch/x86/sse2/simd/function/exp.hpp>
+#  endif
+#endif
 #endif
